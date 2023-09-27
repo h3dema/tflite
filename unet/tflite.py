4df09ce4c6afe60fbd2608b08fce07be16513b41
h3dema <henriquemoura@hotmail.com>
2023-09-27 13:21:35 +0000

    tflite by size

--- unet/tflite.py
+++ unet/tflite.py
@@ -7,6 +7,10 @@ if __name__ == "__main__":
     
     # convert to tflite
     converter = tf.lite.TFLiteConverter.from_saved_model(output_dir)
+    #
+    # see valid optimizations at https://www.tensorflow.org/api_docs/python/tf/lite/Optimize
+    #
+    converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
     tflite_model = converter.convert()
     # save
     tflite_model_file = pathlib.Path(
@@ -14,3 +18,4 @@ if __name__ == "__main__":
     )
     nbytes = tflite_model_file.write_bytes(tflite_model) 
     print(f"Saved tflite model at {tflite_model_file}. Size = {nbytes} bytes")
+    print(f"with optimizations = {converter.optimizations}")
