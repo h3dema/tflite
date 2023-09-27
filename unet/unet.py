4c46052ddc75d07de08058fcc419116ec7e0849c
h3dema <henriquemoura@hotmail.com>
2023-09-27 13:21:15 +0000

    add shallow version

--- unet/unet.py
+++ unet/unet.py
@@ -1,3 +1,7 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # disable all debugging logs 
+
 # for bulding and running deep learning model
 import tensorflow as tf
 from tensorflow.keras.layers import Input
@@ -93,6 +97,13 @@ def DecoderMiniBlock(prev_layer_input, skip_layer_input, n_filters=32):
     return conv
 
 
+# --------------------------------------------
+#
+#  UNET model
+#
+# --------------------------------------------
+
+
 def UNet(input_size=(128, 128, 3), n_filters=32, n_classes=3, dropout_prob=0.3):
     """
         Combine both encoder and decoder blocks according to the U-Net research paper
@@ -106,8 +117,8 @@ def UNet(input_size=(128, 128, 3), n_filters=32, n_classes=3, dropout_prob=0.3):
     cblock1 = EncoderMiniBlock(inputs, n_filters, dropout_prob=0, max_pooling=True)
     cblock2 = EncoderMiniBlock(cblock1[0], n_filters * 2, dropout_prob=0, max_pooling=True)
     cblock3 = EncoderMiniBlock(cblock2[0], n_filters * 4, dropout_prob=0, max_pooling=True)
-    cblock4 = EncoderMiniBlock(cblock3[0], n_filters * 8, dropout_prob=0.3, max_pooling=True)
-    cblock5 = EncoderMiniBlock(cblock4[0], n_filters * 16, dropout_prob=0.3, max_pooling=False)
+    cblock4 = EncoderMiniBlock(cblock3[0], n_filters * 8, dropout_prob=dropout_prob, max_pooling=True)
+    cblock5 = EncoderMiniBlock(cblock4[0], n_filters * 16, dropout_prob=dropout_prob, max_pooling=False)
 
     # Decoder includes multiple mini blocks with decreasing number of filters
     # Observe the skip connections from the encoder are given as input to the decoder
@@ -134,6 +145,61 @@ def UNet(input_size=(128, 128, 3), n_filters=32, n_classes=3, dropout_prob=0.3):
     return model
 
 
+# --------------------------------------------
+#
+#  Shallow UNET model
+#
+# 1------>5
+#  \     /
+#   2-->4 
+#    \ /
+#     3
+# --------------------------------------------
+
+
+def ShallowUNet(input_size=(128, 128, 3), n_filters=32, n_classes=3, dropout_prob=0.3):
+    """
+        Combine both encoder and decoder blocks according to the U-Net research paper
+        Return the model as output
+    """
+    # Input size represent the size of 1 image (the size used for pre-processing)
+    inputs = Input(input_size)
+
+    # Encoder includes multiple convolutional mini blocks with different maxpooling, dropout and filter parameters
+    # Observe that the filters are increasing as we go deeper into the network which will increasse the # channels of the image
+    cblock1 = EncoderMiniBlock(inputs, n_filters, dropout_prob=0, max_pooling=True)
+    cblock2 = EncoderMiniBlock(cblock1[0], n_filters * 2, dropout_prob=0, max_pooling=True)
+    cblock3 = EncoderMiniBlock(cblock2[0], n_filters * 4, dropout_prob=dropout_prob, max_pooling=False)
+
+    # Decoder includes multiple mini blocks with decreasing number of filters
+    # Observe the skip connections from the encoder are given as input to the decoder
+    ublock4 = DecoderMiniBlock(cblock3[0], cblock2[1], n_filters * 2)
+    # Recall the 2nd output of encoder block was skip connection, hence cblockn[1] is used with the upsampling
+    ublock5 = DecoderMiniBlock(ublock4, cblock1[1], n_filters)
+
+    # Complete the model with 1 3x3 convolution layer (Same as the prev Conv Layers)
+    # Followed by a 1x1 Conv layer to get the image to the desired size.
+    # Observe the number of channels will be equal to number of output classes
+    conv9 = Conv2D(n_filters,
+                   3,
+                   activation='relu',
+                   padding='same',
+                   kernel_initializer='he_normal')(ublock5)
+
+    conv10 = Conv2D(n_classes, 1, padding='same')(conv9)
+
+    # Define the model
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
+
+    return model
+
 if __name__ == "__main__":
-    unet = UNet(input_size=(128, 128, 3), n_filters=32, n_classes=3)
-    print(unet.summary())
+    SHALLOW_UNET = True  # True to use shallow Unet (3 levels), False (5 levels)
+    
+    if SHALLOW_UNET:
+        unet = ShallowUNet(input_size=(128, 128, 3), n_filters=32, n_classes=3)
+        print(unet.summary())
+    else:
+        unet = UNet(input_size=(128, 128, 3), n_filters=32, n_classes=3)
+        print(unet.summary())
+
